1b679f0570a5d03ece88558fc9eee4785fc6063c
Stjushenka <stjusheka@gmail.com>
2021-12-02 15:51:22 +0300

    Mile

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,2 +1,13 @@
 public class BonusMilesService {
+    public int price;
+
+    public int calculate(int price) {
+            this.price = price;
+            int bonus = 20;
+            int miles;
+            miles = price / bonus;
+        return miles;
+    }
 }
+
+
