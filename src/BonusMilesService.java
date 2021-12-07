75d33a5fb0521872898696e05c807ae4d58d59a8
Stjushenka <stjusheka@gmail.com>
2021-12-07 14:59:18 +0300

    ref code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -2,10 +2,10 @@ public class BonusMilesService {
     public int price;
 
     public int calculate(int price) {
-            this.price = price;
-            int bonus = 20;
-            int miles;
-            miles = price / bonus;
+        this.price = price;
+        int bonus = 20;
+        int miles;
+        miles = price / bonus;
         return miles;
     }
 }
